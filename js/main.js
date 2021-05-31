073ae7af16c1b4d60b9b4e1500a5cd22ce9505da
camilo huesa <camilowch@gmail.com>
2021-05-31 05:24:19 -0500

    primer commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$("#mostrar").click(function(){
+    $("#caja").show(400);
+});
+$("#ocultar").click(function(){
+    $("#caja").hide(400);
+});
+$("#alerta").click(function(){
+    alert("le diste al boton");
+});
+$("#mostrar2").mouseover(function(){
+    $("#caja3").fadeIn(400);
+});
+$("#ocultar2").mouseover(function(){
+    $("#caja3").fadeOut(400);
+});
+$("#alternar").click(function(){
+    $("#caja4").toggle(300);
+});
